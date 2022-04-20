9671ef74ef0f7be38340f16656bcc5f58e67998c
SergeyHist <sergeyhist@yahoo.com>
2022-04-20 11:33:32 +0300

    mobile footer | main footer and menu fixes

--- src/common.blocks/input/input.js
+++
@@ -1,6 +0,0 @@
-let inputElement = document.querySelector('.input');
-let inputText = document.querySelector('.input__text');
-
-console.log(inputElement);
-console.log(inputText);
-inputText.onclick = function() {inputElement.focus()};
--- src/ui-kit/headers-footers/headers-footers.js
+++ src/ui-kit/headers-footers/headers-footers.js
@@ -1,2 +1,1 @@
 import './headers-footers.scss';
-import '../../common.blocks/input/input.js';
