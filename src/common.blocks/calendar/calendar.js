f5984251fe047b5454317fe216f6f302d3983fe3
SergeyHist <sergeyhist@yahoo.com>
2022-08-05 15:01:38 +0300

    calendar refactoring

--- src/common.blocks/calendar/calendar.js
+++ src/common.blocks/calendar/calendar.js
@@ -7,7 +7,8 @@ let calendarElements = document.querySelectorAll('.calendar');
 function addZero(n) {return n < 10 ? '0'+n : n};
 
 for (let element of calendarElements) {
-  let calendarInputs = element.querySelectorAll('input');
+  let calendarInputs = element.querySelectorAll('.calendar__input');
+  let inputFields = element.querySelectorAll('input');
   let calendarDatepicker = element.querySelector('.calendar__datepicker');
 
   let confirmButton = {
@@ -15,7 +16,7 @@ for (let element of calendarElements) {
     className: 'calendar__button-confirm',
     onClick: (dp) => {
       for (let i of [0,1] )
-        calendarInputs[i].value = addZero(dp.selectedDates[i].getDate())+'.'+addZero(+dp.selectedDates[i].getMonth()+1)+'.'+dp.selectedDates[i].getFullYear(); 
+        inputFields[i].value = addZero(dp.selectedDates[i].getDate())+'.'+addZero(+dp.selectedDates[i].getMonth()+1)+'.'+dp.selectedDates[i].getFullYear(); 
     }
   };
 
@@ -24,7 +25,7 @@ for (let element of calendarElements) {
     className: 'calendar__button-clear',
     onClick: (dp) => {
       dp.clear();
-      for (let input of calendarInputs) {
+      for (let input of inputFields) {
         input.value = '';
       };
     }
@@ -36,27 +37,42 @@ for (let element of calendarElements) {
     buttons: [clearButton, confirmButton]
   });
 
+
   for (let input of calendarInputs) {
-    input.parentElement.onblur = () => {
-      let focusCheck = setInterval(() => {
-        if (!element.contains(document.activeElement) && (document.activeElement != document.body)) {
-          calendarDatepicker.classList.remove('calendar__datepicker_visible');
-          clearInterval(focusCheck);
-        };
-      }, 100);
+    let switchVisibility = (datepicker) => {
+      if (!datepicker.classList.contains('calendar__datepicker_visible')) {
+        datepicker.classList.add('calendar__datepicker_visible');
+      } else {
+        datepicker.classList.remove('calendar__datepicker_visible');
+      };
     };
 
-    input.parentElement.onfocus = () => {
-      calendarDatepicker.classList.add('calendar__datepicker_visible');
+    input.onclick = () => {
+      switchVisibility(calendarDatepicker);  
     };
+
+    input.onkeydown = (e) => {
+      e.key == 'Enter' && switchVisibility(calendarDatepicker);
+    };
+
+    
   };
 
-  document.body.addEventListener('mousedown', (event) => {
+  let inputsArray = Array.from(calendarInputs);
+  let hideCalendar = (e) => {
     if (calendarDatepicker.classList.contains('calendar__datepicker_visible')) {
-      if (!calendarDatepicker.contains(event.target))  {
+      if (!calendarDatepicker.contains(e.target) && !inputsArray.some((input) => input.contains(e.target)))  {
         calendarDatepicker.classList.remove('calendar__datepicker_visible');
       };
     };
+  }
+
+  document.body.addEventListener('mousedown', (e) => {
+    hideCalendar(e);
+  });
+
+  document.body.addEventListener('keydown', (e) => {
+    e.key == 'Enter' && hideCalendar(e);
   });
 
   let navArrows = calendar.$datepicker.querySelectorAll('.air-datepicker-nav--action');
