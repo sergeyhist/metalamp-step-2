3e528bbe2c0e1d3e0e5197c6b90bb4968ed46bfd
SergeyHist <sergeyhist@yahoo.com>
2022-08-09 11:57:33 +0300

    cards > payment card

--- src/card.blocks/payment/payment.js
+++ src/card.blocks/payment/payment.js
@@ -1,6 +1,7 @@
 import './payment.scss';
 import 'common/dropdown/dropdown.js';
 import 'common/calendar/calendar.js';
+import 'common/button/button.js';
 
 let paymentElements = document.querySelectorAll('.payment');
 
@@ -8,7 +9,15 @@ for (let element of paymentElements) {
   let paymentPrice = element.querySelector('.payment__price');
   let paymentDatepicker = element.querySelector('.calendar__datepicker');
   let paymentCost = element.querySelector('.payment__cost');
+  let paymentResult = element.querySelector('.payment__result');
+  let paymentFee = element.querySelector('.payment__fee');
+  let paymentAdditionalFee = element.querySelector('.payment__additional-fee');
 
-  paymentCost.querySelector('.payment__text').textContent = paymentPrice.querySelectorAll('p')[0].textContent+'x '+paymentDatepicker.dataset.selectedDays+' суток';
-  paymentCost.querySelector('.payment__count').textContent = (+paymentPrice.textContent.split('₽')[0] * paymentDatepicker.dataset.selectedDays)+'₽';
+  paymentDatepicker.onchange = () => {
+    paymentCost.querySelector('.payment__text').textContent = paymentPrice.querySelectorAll('p')[0].textContent+'x '+paymentDatepicker.dataset.selectedDays+' суток';
+    paymentCost.querySelector('.payment__count').textContent = (+paymentPrice.textContent.split('₽')[0] * paymentDatepicker.dataset.selectedDays)+'₽';
+    paymentResult.querySelector('.payment__count').textContent = +paymentCost.querySelector('.payment__count').textContent.split('₽')[0] - (+paymentFee.querySelector('.payment__count').textContent.split('₽')[0] + +paymentAdditionalFee.querySelector('.payment__count').textContent.split('₽')[0]) + '₽';
+  };
+
+  paymentDatepicker.dispatchEvent(new Event('change'));
 };
