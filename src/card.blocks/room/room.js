694827013bd4c14b78a881ab02fbcbf90d4813b6
SergeyHist <sergeyhist@yahoo.com>
2022-08-12 11:52:36 +0300

    cards > room card in progress

--- src/card.blocks/room/room.js
+++ src/card.blocks/room/room.js
@@ -1,1 +1,11 @@
 import './room.scss';
+import * as rooms from 'assets/json/rooms.json';
+
+let roomElements = document.querySelectorAll('.room');
+
+for (let element of roomElements) {
+  let roomNumber = element.dataset.number;
+  let selectorItems = element.querySelectorAll('.room__selector-item');
+
+  selectorItems[0].classList.add('room__selector-item_active');
+};
