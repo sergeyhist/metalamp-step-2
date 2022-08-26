b4922ce5041c62547fc3e36e859a3dc6db80c07b
SergeyHist <sergeyhist@yahoo.com>
2022-08-26 10:29:22 +0300

    room-details in progress

---
+++ src/pages/room-details/room-details.js
@@ -0,0 +1,7 @@
+import './room-details.scss';
+import 'common/header/header.js';
+import 'common/footer/footer.js';
+import 'common/list/list.js';
+import 'common/comment/comment.js';
+import 'common/bullet/bullet.js';
+import 'cards/payment/payment.js';
