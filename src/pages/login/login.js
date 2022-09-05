10ca3e622a6efc8e4d0386061eb4a06f9a33006f
SergeyHist <sergeyhist@yahoo.com>
2022-09-05 12:54:15 +0300

    login/registration pages

---
+++ src/pages/login/login.js
@@ -0,0 +1,5 @@
+import './login.scss';
+import 'common/header/header.js';
+import 'common/footer/footer.js';
+import 'common/slideshow/slideshow.js';
+import 'cards/login/login.js';
---
+++ src/pages/registration/registration.js
@@ -0,0 +1,5 @@
+import './registration.scss';
+import 'common/header/header.js';
+import 'common/footer/footer.js';
+import 'common/slideshow/slideshow.js';
+import 'cards/registration/registration.js';
