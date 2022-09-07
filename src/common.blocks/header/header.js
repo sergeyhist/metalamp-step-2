50e8bc03bd61524b652fc09009a9c29a89365db9
SergeyHist <sergeyhist@yahoo.com>
2022-09-07 11:01:22 +0300

    adaptive header in progress

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,3 +1,20 @@
 import './header.scss';
 import 'common/button/button.scss';
-import 'common/account/account.scss';
+
+let headerElements = document.querySelectorAll('.header');
+
+for (let element of headerElements) {
+  let mobileMenu = element.querySelector('.header__mobile-menu');
+  let menu = element.querySelector('.header__menu');
+  let switchVisibility = () => {
+    if (!menu.classList.contains('header__menu_visible')) {
+      menu.classList.add('header__menu_visible');
+    } else {
+      menu.classList.remove('header__menu_visible');
+    };
+  };
+
+  mobileMenu.onclick = () => {
+    switchVisibility();
+  };
+};
