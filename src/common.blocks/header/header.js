cc9ddf320e27b71564537ce3225c05e62d838d69
SergeyHist <sergeyhist@yahoo.com>
2022-09-08 13:56:13 +0300

    adaptive header

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -6,7 +6,10 @@ let headerElements = document.querySelectorAll('.header');
 for (let element of headerElements) {
   let mobileMenu = element.querySelector('.header__mobile-menu');
   let menu = element.querySelector('.header__menu');
-  let switchVisibility = () => {
+  let arrowMenuLinks = Array.from(element.querySelectorAll('.header__menu-link_arrow'));
+  let submenuElements = Array.from(element.querySelectorAll('.header__submenu'));
+
+  let switchMenuVisibility = () => {
     if (!menu.classList.contains('header__menu_visible')) {
       menu.classList.add('header__menu_visible');
     } else {
@@ -14,7 +17,38 @@ for (let element of headerElements) {
     };
   };
 
+  let switchSubmenuVisibility = (i) => {
+    if (!submenuElements[i].classList.contains('header__submenu_visible')) {
+      
+      submenuElements[i].classList.add('header__submenu_visible');
+    } else {
+      submenuElements[i].classList.remove('header__submenu_visible');
+    };
+  };
+
   mobileMenu.onclick = () => {
-    switchVisibility();
+    switchMenuVisibility();
+  };
+
+  mobileMenu.onkeydown = (e) => {
+    e.key == 'Enter' && switchMenuVisibility();
+  };
+
+  document.addEventListener('mousedown', (event) => {
+    if (!mobileMenu.contains(event.target) && !menu.contains(event.target)) {
+      menu.classList.remove('header__menu_visible');
+    };
+  });
+
+  for (let i in arrowMenuLinks) {
+    arrowMenuLinks[i].onclick = () => {
+      switchSubmenuVisibility(i);
+    };
+
+    document.body.addEventListener('mousedown', (event) => {
+      if (!arrowMenuLinks[i].contains(event.target) && !submenuElements[i].contains(event.target)) {
+        submenuElements[i].classList.remove('header__submenu_visible');
+      };
+    });
   };
 };
