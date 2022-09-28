1bbde75fe3d67bdb23831478efb54c00f6209ec4
SergeyHist <sergeyhist@yahoo.com>
2022-09-28 12:38:29 +0300

    few fixes | adaptive landing | remove slideshow

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,27 +1,36 @@
 import './landing.scss';
 import 'common/header/header.js';
 import 'common/footer/footer.js';
-import 'common/slideshow/slideshow.js';
 import 'cards/search/search.js';
+import slide1 from 'assets/images/slide_1.png';
+import slide2 from 'assets/images/slide_2.png';
 
-let pageText = document.querySelector('.page__text')
+let pageText = document.querySelector('.page__text');
+let pageSlideshow = document.querySelector('.page__slideshow');
 
 pageText.classList.add('page__text_bottom');
+pageSlideshow.style.backgroundImage = `url(${slide2})`;
 
 setInterval(() => {
   if (pageText.classList.contains('page__text_top')) {
     pageText.style.opacity = 0;
+    pageSlideshow.style.opacity = 0;
     setTimeout(() => {
       pageText.classList.remove('page__text_top');
       pageText.classList.add('page__text_bottom');
+      pageSlideshow.style.backgroundImage = `url(${slide2})`;
       pageText.style.opacity = 1;
+      pageSlideshow.style.opacity = 1;
     }, 1000);
   } else {
     pageText.style.opacity = 0;
+    pageSlideshow.style.opacity = 0;
     setTimeout(() => {
       pageText.classList.remove('page__text_bottom');
       pageText.classList.add('page__text_top');
+      pageSlideshow.style.backgroundImage = `url(${slide1})`;
       pageText.style.opacity = 1;
-    }, 1000);
+      pageSlideshow.style.opacity = 1;
+    }, 500);
   };
 }, 10000);
--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,5 +1,4 @@
 import './registration.scss';
 import 'common/header/header.js';
 import 'common/footer/footer.js';
-import 'common/slideshow/slideshow.js';
 import 'cards/registration/registration.js';
